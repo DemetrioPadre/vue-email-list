6fe228c9b44b3021f5ff1df01c857540badc9dad
DemetrioPadre <demetriopadre@gmail.com>
2024-02-06 16:46:58 +0100

    add vue

--- data.js
+++ data.js
@@ -1,6 +1,24 @@
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            title: 'ciao Padrone sono qua per servirti ed onorarti',
+        };
+    },
+
+    methods: {},
+
+    mounted() { },
+});
+
+app.mount('#app');
+
 for (let i = 0; i < 10; i++) {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-
-        console.table(response.data.response);
+        const randomEmail = response.data.response;
+        console.table(randomEmail);
     });
 }
+
+
