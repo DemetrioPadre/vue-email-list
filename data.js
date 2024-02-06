24993320d3bb3d847a77a35cb7f063afa58f2a63
DemetrioPadre <demetriopadre@gmail.com>
2024-02-06 17:32:51 +0100

    add css

--- data.js
+++ data.js
@@ -3,22 +3,26 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            title: 'ciao Padrone sono qua per servirti ed onorarti',
+            title: 'ciao Padrone sono qua per servirti ed onorarti, ecco a te le randoMail richieste ma se apri la console le troverai anche li',
+            list: [],
         };
     },
 
-    methods: {},
+    methods() { },
+    created() {
 
-    mounted() { },
+        for (let i = 0; i < 12; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                console.table(response.data.response);
+                this.list.push(response.data.response);
+
+            });
+        }
+    }
+
 });
 
 app.mount('#app');
 
-for (let i = 0; i < 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-        const randomEmail = response.data.response;
-        console.table(randomEmail);
-    });
-}
 
 
